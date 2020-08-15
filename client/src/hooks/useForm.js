abce013ec02c7501c9307ba5f71e71001ea82b3e
Melanie Mendoza <melanie.mendoza@gmail.com>
2020-08-14 21:44:31 -0400

    MVP

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export function useForm(initialValue) {
+    // define state
+    const [value, setValue] = useState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+    // define setter function
+    const handleChanges = (e) => {
+        setValue({ ...value, [e.target.name]: e.target.value });
+        console.log(value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    return [value, handleChanges, handleSubmit, showSuccessMessage]
+};
